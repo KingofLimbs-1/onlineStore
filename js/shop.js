1cb8becf6ca45c17820ae9d156972d5615262da1
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-05-02 23:29:16 +0200

    Continued working on "shop" page
    1. Condensed all the "createProductCards" functions that all did similiar things into one single function that iterates over each product section and creates their respective product cards.
    
    2. Created a modal section within the pages HTML.
    
    3. Created functionality that dynamically creates modals for the products when they are clicked on.
    
    4. Styled product modals.

--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,6 @@
 /* --- REFERENCES --- */
 
 // ARRAYS
-const productArray = [];
 const cartArray = [];
 // ...
 
@@ -12,28 +11,41 @@ const cartArray = [];
 
 /* --- FUNCTIONS --- */
 
-// LIVING ROOM POPULATION FUNCTION
-// JSON import
+// Create product cards function
+
 fetch("/json/data.json")
   .then((response) => response.json())
   .then((data) => {
-    // do something with the data here
-    const livingRoomProducts = data.livingRoom;
+    // Product section function calls with "selector" and "data" arguments
+    const livingRoomCards = createProductCards(
+      ".livingRoomContainer",
+      data.livingRoom
+    );
+    const diningRoomCards = createProductCards(
+      ".diningRoomContainer",
+      data.diningRoom
+    );
+    const officeCards = createProductCards(".officeContainer", data.office);
+    const decorCards = createProductCards(".decorContainer", data.decor);
 
-    createLivingCards(livingRoomProducts);
+    // Returned function calls for each section
+    livingRoomCards();
+    diningRoomCards();
+    officeCards();
+    decorCards();
   })
   .catch((error) => console.error(error));
 
-function createLivingCards(livingRoomProducts) {
-  // Section reference
-  const livingRoom = document.querySelector(".livingRoomContainer");
+// createProductCards function
+function createProductCards(selector, data) {
+  // Product section value assignment
+  const section = document.querySelector(selector);
 
-  livingRoomProducts.forEach((product) => {
+  data.forEach((product) => {
     // Product card div creation
     // This is the parent div for products
     const productCard = document.createElement("div");
     productCard.className = "productCard";
-    // livingRoom.classList.add(productCard);
 
     // Container div creation
     // Text will be stored within this div
@@ -58,164 +70,72 @@ function createLivingCards(livingRoomProducts) {
     container.appendChild(productPrice);
 
     // Product card add to product section
-    livingRoom.appendChild(productCard);
+    section.appendChild(productCard);
     // Container add to productCard div
     productCard.appendChild(container);
+
+    // Modal creation
+    productCard.addEventListener("click", function () {
+      // Modal container reference
+      const modalContainer = document.querySelector(".modalContainer");
+      //   Display logic
+      if (
+        modalContainer.style.display === "none" ||
+        modalContainer.style.display === ""
+      ) {
+        modalContainer.style.display = "block";
+      }
+
+      // Modal header reference
+      const modalHeader = document.querySelector(".modalHeader");
+      // Modal section reset
+      modalHeader.innerHTML = "";
+      // Heading creation
+      const heading = document.createElement("h2");
+      // Heading value assignment
+      heading.textContent = product.name;
+      modalHeader.appendChild(heading);
+      // Close button creation
+      const closeBtn = document.createElement("button");
+      closeBtn.className = "closeBtn";
+      closeBtn.innerHTML = '<img src="/images/shopPage/closeBtn.png"/>';
+      heading.appendChild(closeBtn);
+      closeBtn.addEventListener("click", function () {
+        closeModal();
+      });
+
+      // Modal body reference
+      const modalBody = document.querySelector(".modalBody");
+      //   Modal section reset
+      modalBody.innerHTML = "";
+      // Image creation
+      const productImg = document.createElement("img");
+      productImg.setAttribute("src", product.image);
+      productImg.setAttribute("alt", product.image);
+      productImg.className = "modalImage";
+      modalBody.appendChild(productImg);
+
+      //   Modal footer reference
+      const modalFooter = document.querySelector(".modalFooter");
+      modalFooter.innerHTML = "";
+      // Add to cart button creation
+      const addBtn = document.createElement("button");
+      addBtn.textContent = "Add to Cart";
+      modalFooter.appendChild(addBtn);
+    });
+    // ...
+
+    // Close modal function 
+    function closeModal() {
+      const modalContainer = document.querySelector(".modalContainer");
+      if (modalContainer.style.display === "block") {
+        modalContainer.style.display = "none";
+      }
+    }
   });
 }
-// ...
-
-// DINING ROOM POPULATION FUNCTION
-fetch("/json/data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    // do something with the data here
-    const diningRoomProducts = data.diningRoom;
-
-    createDiningCards(diningRoomProducts);
-  })
-  .catch((error) => console.error(error));
-
-function createDiningCards(diningRoomProducts) {
-  // Section reference
-  const diningRoom = document.querySelector(".diningRoomContainer");
-
-  diningRoomProducts.forEach((product) => {
-    // Product card div creation
-    // This is the parent div for products
-    const productCard = document.createElement("div");
-    productCard.className = "productCard";
-    // livingRoom.classList.add(productCard);
-
-    // Container div creation
-    // Text will be stored within this div
-    const container = document.createElement("div");
-    container.className = "container";
-
-    // Image creation
-    // This will be stored within productCard, outside of container
-    const productImage = document.createElement("img");
-    productImage.setAttribute("src", product.image);
-    productImage.setAttribute("alt", product.image);
-    productCard.appendChild(productImage);
-
-    // Name creation
-    const productName = document.createElement("h3");
-    productName.textContent = product.name;
-    container.appendChild(productName);
-
-    // Price creation
-    const productPrice = document.createElement("span");
-    productPrice.textContent = product.price;
-    container.appendChild(productPrice);
-
-    // Product card add to product section
-    diningRoom.appendChild(productCard);
-    // Container add to productCard div
-    productCard.appendChild(container);
-  });
-}
-// ...
-
-// OFFICE POPULATION FUNCTION
-fetch("/json/data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    // do something with the data here
-    const officeProducts = data.office;
-
-    createOfficeCards(officeProducts);
-  })
-  .catch((error) => console.error(error));
-
-function createOfficeCards(officeProducts) {
-  // Section reference
-  const office = document.querySelector(".officeContainer");
-
-  officeProducts.forEach((product) => {
-    // Product card div creation
-    // This is the parent div for products
-    const productCard = document.createElement("div");
-    productCard.className = "productCard";
-
-    // Container div creation
-    // Text will be stored within this div
-    const container = document.createElement("div");
-    container.className = "container";
-
-    // Image creation
-    // This will be stored within productCard, outside of container
-    const productImage = document.createElement("img");
-    productImage.setAttribute("src", product.image);
-    productImage.setAttribute("alt", product.image);
-    productCard.appendChild(productImage);
-
-    // Name creation
-    const productName = document.createElement("h3");
-    productName.textContent = product.name;
-    container.appendChild(productName);
-
-    // Price creation
-    const productPrice = document.createElement("span");
-    productPrice.textContent = product.price;
-    container.appendChild(productPrice);
-
-    // Product card add to product section
-    office.appendChild(productCard);
-    // Container add to productCard div
-    productCard.appendChild(container);
-  });
-}
-// ...
-
-// DECOR POPULATI0N FUNCTION
-fetch("/json/data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    // do something with the data here
-    const decorProducts = data.decor;
-
-    createDecorCards(decorProducts);
-  })
-  .catch((error) => console.error(error));
-
-  function createDecorCards(decorProducts){
-    // Section reference
-  const decor = document.querySelector(".decorContainer");
-
-  decorProducts.forEach((product) => {
-    // Product card div creation
-    // This is the parent div for products
-    const productCard = document.createElement("div");
-    productCard.className = "productCard";
-
-    // Container div creation
-    // Text will be stored within this div
-    const container = document.createElement("div");
-    container.className = "container";
-
-    // Image creation
-    // This will be stored within productCard, outside of container
-    const productImage = document.createElement("img");
-    productImage.setAttribute("src", product.image);
-    productImage.setAttribute("alt", product.image);
-    productCard.appendChild(productImage);
-
-    // Name creation
-    const productName = document.createElement("h3");
-    productName.textContent = product.name;
-    container.appendChild(productName);
-
-    // Price creation
-    const productPrice = document.createElement("span");
-    productPrice.textContent = product.price;
-    container.appendChild(productPrice);
-
-    // Product card add to product section
-    decor.appendChild(productCard);
-    // Container add to productCard div
-    productCard.appendChild(container);
-  });
-  }
 // ...
 /* ---/ FUNCTIONS /--- */
+
+/* --- EVENT LISTENERS --- */
+/* ---/ EVENT LISTENERS /--- */

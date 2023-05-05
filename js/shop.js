93e5a292a8b5d92863fc1acf2cee14a8de83d638
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-05-05 19:04:53 +0200

    Continued working on "shop" page
    1. Created a section for the cart modal & added navbar HTML from homepage to shop page.
    
    2. Updated JSON data with "descriptions" and "ratings" for products.
    
    3. Updated product modals with product descriptions and ratings. Began building the cart functionality (addToCart, updateCart, createCartItems). These functions make up the core functionality of the cart logic.
    
    4. Added some styling changes to the "addToCart" button and added navbar styling.

--- js/shop.js
+++ js/shop.js
@@ -1,13 +1,30 @@
 /* --- REFERENCES --- */
 
-// ARRAYS
+// Product modal container
+const modalContainer = document.querySelector(".modalContainer");
+
+// Product modal header
+const modalHeader = document.querySelector(".modalHeader");
+
+// Product modal body
+const modalBody = document.querySelector(".modalBody");
+
+// Product modal footer
+const modalFooter = document.querySelector(".modalFooter");
+
+// Cart array
 const cartArray = [];
-// ...
+
+// Cart button
+const cartBtn = document.querySelector(".cart");
+
+// Cart modal container
+const cartModalContainer = document.querySelector(".cartModalContainer");
+
+// Cart modal content
+const cartModalContent = document.querySelector(".cartModalContent");
 
 /* ---/ REFERENCES /--- */
-
-/* --- OBJECTS/CLASSES --- */
-/* ---/ OBJECTS/CLASSES /--- */
 
 /* --- FUNCTIONS --- */
 
@@ -41,6 +58,7 @@ function createProductCards(selector, data) {
   // Product section value assignment
   const section = document.querySelector(selector);
 
+  // Product card creation
   data.forEach((product) => {
     // Product card div creation
     // This is the parent div for products
@@ -73,11 +91,10 @@ function createProductCards(selector, data) {
     section.appendChild(productCard);
     // Container add to productCard div
     productCard.appendChild(container);
+    // ...
 
-    // Modal creation
+    // Product modal creation
     productCard.addEventListener("click", function () {
-      // Modal container reference
-      const modalContainer = document.querySelector(".modalContainer");
       //   Display logic
       if (
         modalContainer.style.display === "none" ||
@@ -86,8 +103,8 @@ function createProductCards(selector, data) {
         modalContainer.style.display = "block";
       }
 
-      // Modal header reference
-      const modalHeader = document.querySelector(".modalHeader");
+      // Product modal header element creation
+
       // Modal section reset
       modalHeader.innerHTML = "";
       // Heading creation
@@ -95,17 +112,31 @@ function createProductCards(selector, data) {
       // Heading value assignment
       heading.textContent = product.name;
       modalHeader.appendChild(heading);
+      // Description creation
+      const description = document.createElement("p");
+      description.textContent = product.description;
+      description.className = "description";
+      modalHeader.appendChild(description);
+      // Rating creation
+      const rating = document.createElement("img");
+      rating.src = product.rating;
+      rating.className = "rating";
+      const ratingContainer = document.createElement("div");
+      ratingContainer.className = "ratingContainer";
+      ratingContainer.appendChild(rating);
+      modalHeader.appendChild(ratingContainer);
       // Close button creation
       const closeBtn = document.createElement("button");
       closeBtn.className = "closeBtn";
-      closeBtn.innerHTML = '<img src="/images/shopPage/closeBtn.png"/>';
+      closeBtn.innerHTML = '<img src="/images/shopPage/close.png"/>';
       heading.appendChild(closeBtn);
+      // Close button event listener that closes modal once button is clicked
       closeBtn.addEventListener("click", function () {
         closeModal();
       });
 
-      // Modal body reference
-      const modalBody = document.querySelector(".modalBody");
+      // Product modal body element creation
+
       //   Modal section reset
       modalBody.innerHTML = "";
       // Image creation
@@ -117,27 +148,105 @@ function createProductCards(selector, data) {
         modalBody.appendChild(productImg);
       });
 
-      //   Modal footer reference
-      const modalFooter = document.querySelector(".modalFooter");
+      // Product modal footer element creation
+
       modalFooter.innerHTML = "";
       // Add to cart button creation
       const addBtn = document.createElement("button");
       addBtn.textContent = "Add to Cart";
+      addBtn.className = "addBtn";
       modalFooter.appendChild(addBtn);
+      // addToCart function call event
+      addBtn.addEventListener("click", function () {
+        addToCart(product);
+      });
+      // ...
     });
-    // ...
-
-    // Close modal function
-    function closeModal() {
-      const modalContainer = document.querySelector(".modalContainer");
-      if (modalContainer.style.display === "block") {
-        modalContainer.style.display = "none";
-      }
-    }
   });
 }
 // ...
+
+// Close modal function
+function closeModal() {
+  if (modalContainer.style.display === "block") {
+    modalContainer.style.display = "none";
+  }
+}
+// ...
+
+// Add to cart function
+function addToCart(product) {
+  // Check to see if the product is already in the cart
+  const index = cartArray.findIndex((item) => item.name === product.name);
+  if (index !== -1) {
+    // If the product is already in the cart, increase its number of units
+    alert("That product is already in your cart!");
+  } else {
+    cartArray.push({
+      // New object attribute creation for quantity incrementation
+      ...product,
+      numberOfUnits: 1,
+    });
+  }
+  updateCart();
+}
+// ...
+
+// Update cart function
+function updateCart() {
+  // Create cart items function call
+  createCartItems();
+}
+// ...
+
+// Create cart items function
+function createCartItems() {
+  cartModalContent.innerHTML = "";
+  cartArray.forEach((product) => {
+    cartModalContent.innerHTML += `
+    <div class="cartModalBody">
+      <div class="imageContainer">
+        <img src="${product.image}" alt="${product.name}">
+      </div>
+      <div class="nameContainer">
+        <h1>${product.name}</h1>
+      </div>
+      <div class="priceContainer">
+        <span>${product.price}</span>
+      </div>
+    </div>
+    <div class="cartModalFooter">
+      <div class="minusBtn">
+      <img src="/images/shopPage/minusBtn.png" alt="">
+      </div>
+      <div class="units">${product.numberOfUnits}</div>
+      <div class="plusBtn">
+      <img src="/images/shopPage/plusBtn.png" alt="">
+      </div>
+    </div>
+    `;
+  });
+}
+// ...
+
+// Show cart
+function showCart() {
+  if (
+    cartModalContainer.style.display === "none" ||
+    cartModalContainer.style.display === ""
+  ) {
+    cartModalContainer.style.display = "block";
+  }
+}
+// ...
+
 /* ---/ FUNCTIONS /--- */
 
 /* --- EVENT LISTENERS --- */
+
+// Cart button event
+cartBtn.addEventListener("click", function () {
+  showCart();
+});
+// ...
 /* ---/ EVENT LISTENERS /--- */

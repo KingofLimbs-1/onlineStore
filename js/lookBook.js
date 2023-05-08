44ded37726b2d783559ddbb1ae680f6d04c60217
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-05-08 04:07:44 +0200

    Finished working on "look book" page
    1. Created gallery sections markup and mobile nav markup.
    
    2. Added cart persistence functionality.
    
    3. Added mobile nav functionality.
    
    4. Added media queries.

--- js/lookBook.js
+++ js/lookBook.js
@@ -1,25 +1,55 @@
 /* --- Element References --- */
 // Navbar
 const navBar = document.querySelector(".rowContainer");
+// Cart item count
+const cartQuantity = document.querySelector(".quantity");
+// Mobile navbar
+const mobileMenu = document.querySelector(".mobileMenu");
+const navBarItems = document.querySelector(".navBarItems");
 /* ---/ Element References /--- */
 
 /* --- Functions --- */
 // Sticky navbar
 function stickyNav() {
-    if (window.pageYOffset >= sticky) {
-      navBar.classList.add("sticky")
-    } else {
-      navBar.classList.remove("sticky");
-    }
+  if (window.pageYOffset >= sticky) {
+    navBar.classList.add("sticky");
+  } else {
+    navBar.classList.remove("sticky");
   }
-  
-  const sticky = navBar.offsetTop;
-  
-  // When the user scrolls on the page, stickyNav will be executed
-  window.onscroll = function() {stickyNav()};
-  // ...
+}
+
+const sticky = navBar.offsetTop;
+
+// When the user scrolls on the page, stickyNav will be executed
+window.onscroll = function () {
+  stickyNav();
+};
+// ...
+
+// Get cart from local storage function
+function getCartFromLocalStorage() {
+  const cartData = localStorage.getItem("cart");
+  if (cartData) {
+    return JSON.parse(cartData);
+  }
+}
+
+// Get cart quantity from local storage
+function getCartItemCount() {
+  const cartData = localStorage.getItem("cart");
+  if (cartData) {
+    const cartArray = JSON.parse(cartData);
+    return cartArray.length;
+  }
+}
+
+const itemCount = getCartItemCount();
+cartQuantity.innerText = itemCount.toString();
 /* ---/ Functions /--- */
 
-
 /* --- Events --- */
+// Mobile responsive navbar event
+mobileMenu.addEventListener('click', () => {
+  navBarItems.classList.toggle('active');
+});
 /* ---/ Events /--- */

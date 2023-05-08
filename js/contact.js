e6e4863797deb97197e4da057c02c09f5a709e38
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-05-08 03:27:22 +0200

    Finished working on "contact" page
    1. Updated cart element mark-up.
    
    2. Added "mobile navbar" mark up.
    
    3. Added cart persistence JS functionality to page.
    
    4. Added mobile nav functionality.
    
    5. Changed necessary styling.

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,11 @@
 /* --- Element References --- */
 // Navbar
 const navBar = document.querySelector(".rowContainer");
+// Cart item count
+const cartQuantity = document.querySelector(".quantity");
+// Mobile navbar
+const mobileMenu = document.querySelector(".mobileMenu");
+const navBarItems = document.querySelector(".navBarItems");
 /* ---/ Element References /--- */
 
 /* --- Functions --- */
@@ -18,8 +23,32 @@ function stickyNav() {
   // When the user scrolls on the page, stickyNav will be executed
   window.onscroll = function() {stickyNav()};
   // ...
+
+  // Get cart from local storage function
+function getCartFromLocalStorage() {
+  const cartData = localStorage.getItem("cart");
+  if (cartData) {
+    return JSON.parse(cartData);
+  }
+}
+
+// Get cart quantity from local storage
+function getCartItemCount() {
+  const cartData = localStorage.getItem("cart");
+  if (cartData) {
+    const cartArray = JSON.parse(cartData);
+    return cartArray.length;
+  }
+}
+
+const itemCount = getCartItemCount();
+cartQuantity.innerText = itemCount.toString();
 /* ---/ Functions /--- */
 
 
 /* --- Events --- */
+// Mobile responsive navbar event
+mobileMenu.addEventListener('click', () => {
+  navBarItems.classList.toggle('active');
+});
 /* ---/ Events /--- */
